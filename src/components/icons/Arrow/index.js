1adba9dfae81a0d81573818fd07ce804ef8def10
Ira Aspen <zebington@gmail.com>
2018-04-26 14:01:05 +0100

    Added up mode for arrow icon.

--- src/components/icons/Arrow/index.js
+++ src/components/icons/Arrow/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './Arrow.css';
 
-const Arrow = () => <svg viewBox="0 0 90 50" xmlns="http://www.w3.org/2000/svg" className="icons icons-arrow">
-    <path d="M0 0 45 50 90 0Z"/>
-</svg>;
+const Arrow = props => {
+    const path = props.up ?
+        "M0 50 45 0 90 50Z"
+        : "M0 0 45 50 90 0Z";
+    return <svg viewBox="0 0 90 50" xmlns="http://www.w3.org/2000/svg" className="icons icons-arrow">
+        <path d={path}/>
+    </svg>;
+};
 
 export default Arrow;
